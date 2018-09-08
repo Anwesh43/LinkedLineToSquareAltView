5c07ce1ba3fd784e246e7be8998fc6a7a4a7a9d7
Anwesh43 <anweshthecool0@gmail.com>
2018-09-08 13:01:48 +0530

    using renderer in view

--- linetosquareview/src/main/java/com/anwesh/uiprojects/linetosquareview/LineToSquareLineView.kt
+++ linetosquareview/src/main/java/com/anwesh/uiprojects/linetosquareview/LineToSquareLineView.kt
@@ -35,16 +35,18 @@ fun Canvas.drawSLANode(i : Int, scale : Float, paint : Paint) {
 
 class LineToSquareLineView(ctx : Context) : View(ctx) {
 
+    private val renderer : Renderer = Renderer(this)
+
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     override fun onDraw(canvas : Canvas) {
-
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
