6d46d30f1d841e31d1adfc3424ddd420beeeb438
Anwesh43 <anweshthecool0@gmail.com>
2018-09-08 13:16:53 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedlinetosquarealtview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlinetosquarealtview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedlinetosquarealtview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.linetosquareview.LineToSquareLineView
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +10,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         LineToSquareLineView.create(this)
+        fullScreen()
     }
 }
 
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+}
 
 
