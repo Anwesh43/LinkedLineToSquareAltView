b35ad419125b2a41eddc3d18ef84bf0fa492c523
Anwesh43 <anweshthecool0@gmail.com>
2018-09-08 14:36:37 +0530

    adding OOnMovementListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedlinetosquarealtview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlinetosquarealtview/MainActivity.kt
@@ -3,14 +3,24 @@ package com.anwesh.uiprojects.linkedlinetosquarealtview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.linetosquareview.LineToSquareLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineToSquareLineView.create(this)
+        val view : LineToSquareLineView = LineToSquareLineView.create(this)
         fullScreen()
+        view.addOnMovementListener ({i ->
+            createToast("movement with index ${i} completed")
+        }, {i ->
+            createToast("movement with index ${i} is reset")
+        })
+    }
+
+    fun createToast(msg : String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 }
 
