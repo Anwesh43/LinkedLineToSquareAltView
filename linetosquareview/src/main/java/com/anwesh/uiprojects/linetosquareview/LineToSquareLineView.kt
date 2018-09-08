78182bdd593deadd46ba2e94b74316d955bc0ff7
Anwesh43 <anweshthecool0@gmail.com>
2018-09-08 12:14:16 +0530

    importing libraries and defining nodes

--- linetosquareview/src/main/java/com/anwesh/uiprojects/linetosquareview/LineToSquareLineView.kt
+++ linetosquareview/src/main/java/com/anwesh/uiprojects/linetosquareview/LineToSquareLineView.kt
@@ -3,3 +3,13 @@ package com.anwesh.uiprojects.linetosquareview
 /**
  * Created by anweshmishra on 08/09/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+
+val nodes : Int = 5
+
